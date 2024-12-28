5af4b47937a3a9ebbda8dad9806a7acfc6988742
Cytezera <ezrachin05@gmail.com>
2024-12-28 21:21:02 +0800

    Created a window

--- main.cpp
+++ main.cpp
@@ -10,10 +10,27 @@ int main( int argc, char* args[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0 ){
 		cout << "SDL could not be initialized " << SDL_GetError() ; 
 	}else {
-		window = SDL_CreateWindow("SDL Tutorial" , SDL_WINDOWPOS_UNDEFINED , SDL_WINODWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("SDL Tutorial" , SDL_WINDOWPOS_UNDEFINED , SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL ) {
 			cout << "winodw could not be created " << endl; 
 		}
+		else {
+			screenSurface = SDL_GetWindowSurface(window); 
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB(screenSurface->format,0xFF,0xFF,0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Event e ; 
+			bool quit = false; 
+			while (quit == false )
+			{
+				while (SDL_PollEvent( &e))
+				{
+					if (e.type == SDL_QUIT) quit = true; 
+				}
+			}
+		}
 	}
+	SDL_DestroyWindow( window); 
+	SDL_Quit();
+	return 0 ; 
 
 }
