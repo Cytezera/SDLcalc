2f8a9143edc2273c4d55077f0f0fa3be8497919e
Cytezera <ezrachin05@gmail.com>
2024-12-31 00:35:51 +0800

    Added text into test button

--- main.cpp
+++ main.cpp
@@ -11,14 +11,42 @@ struct Button {
 	SDL_Rect rect; 
 	SDL_Color color; 
 	string label ;
+	SDL_Color labelColour;
 }; 
-void createButton(SDL_Renderer* renderer , TTF_Font* font ){
+void buttonText(SDL_Renderer* renderer, TTF_Font* font , Button& button) {
+	button.labelColour = {255,255,255,255};	
+	SDL_Surface* textSurface = TTF_RenderText_Blended(font, button.label.c_str(), button.labelColour); 
+	if (!textSurface){
+		cerr << "Unable to produce text surface " << SDL_GetError() << endl; 
+		return ;
+	}
+	
+	SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface); 
+	if (!textTexture){
+		cerr << "Unable to produce text texture " << SDL_GetError() << endl; 
+	}		
+	int textWidth = textSurface->w;
+	int textHeight = textSurface->h; 
+	SDL_Rect textRect  = {
+		button.rect.x + (button.rect.w - textWidth) / 2 , 
+		button.rect.y + (button.rect.h - textHeight) / 2,
+		textWidth,
+		textHeight
+	};
+	SDL_RenderCopy(renderer, textTexture, nullptr, &textRect); 
+	SDL_FreeSurface(textSurface);
+	SDL_DestroyTexture(textTexture) ;
+
+};
+void createButton(SDL_Renderer* renderer , TTF_Font* font){
 	Button button ;
 	button.rect = {300, 200 , 200 ,100 } ; 
 	button.color = {0,128,255,255}; 
 	button.label = "Click me!" ; 	
+
 	SDL_SetRenderDrawColor(renderer,button.color.r, button.color.g , button.color.b , button.color.a);
 	SDL_RenderFillRect(renderer, &button.rect); 	
+	buttonText(renderer, font, button ) ; 
 	SDL_RenderPresent(renderer); 
 }
 void openWindow(){
@@ -62,7 +90,6 @@ void openWindow(){
 			}
 		}
 		SDL_SetRenderDrawColor(renderer, 255, 255 ,255 ,255 ) ; 
-		SDL_RenderClear(renderer); 
 		createButton(renderer,font );
 	}
 	TTF_CloseFont(font); 
